42009f853e566947b001533f62d7aa0e3ad2157c
An Truong <truongngocan5901@hotmail.com>
2022-02-26 21:56:40 +1030

    change css

--- dashboard.js
+++ dashboard.js
@@ -2,6 +2,20 @@ function log_in() {
     var email = document.getElementById("log_in_email").value;
     var password = document.getElementById("log_in_password").value;
 
+    const submit = document.getElementById("submit");
+
+    submit.addEventListener("click", validate);
+
+    const email = document.getElementById("mail");
+
+    email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter your email address here");
+    email.reportValidity();
+    } else {
+        email.setCustomValidity("");
+    }
+    });
     if (email == "sample@gmail.com") {
         if (password == "sample") {
             window.location.href = "dashboard_unscanned.html";
@@ -14,5 +28,40 @@ function log_in() {
         window.location.href = "login.html";
         return;
     }
+
+}
+
+function signup() {
+    var email = document.getElementById("signup_email").value;
+    var password = document.getElementById("signup_password").value;
+
+    const submit = document.getElementById("submit");
+
+    submit.addEventListener("click", validate);
+
+    const email = document.getElementById("mail");
+
+    email.addEventListener("input", function (event) {
+    if (email.validity.typeMismatch) {
+    email.setCustomValidity("Please enter your email address here");
+    email.reportValidity();
+    } else {
+        email.setCustomValidity("");
+    }
+    });
+
+    if (email == "sample@gmail.com") {
+        if (password == "sample") {
+            window.location.href = "dashboard_unscanned.html";
+            return;
+        } else {
+            window.location.href = "signup.html";
+            return;
+        }
+    } else {
+        window.location.href = "signup.html";
+        return;
+    }
+    
 
 }
